6c4277553582cb6c2274a54ffb539a4e93cc9b53
Folajimmie <hayodele24@gmail.com>
2022-04-15 05:22:07 +0100

    Updates

---
+++ precache-manifest.d12723fe8bd15d9cee063925e0fea860.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61eeae34b7eb75de373d",
+    "url": "/form-project/static/css/main.4e035c79.chunk.css"
+  },
+  {
+    "revision": "61eeae34b7eb75de373d",
+    "url": "/form-project/static/js/main.61eeae34.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/form-project/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "aab73082194fee1f473f",
+    "url": "/form-project/static/js/2.aab73082.chunk.js"
+  },
+  {
+    "revision": "7f937ed823dd685d2e4e",
+    "url": "/form-project/static/js/runtime~main.7f937ed8.js"
+  },
+  {
+    "revision": "241afc13d8539e60d2f40b655ebc2f6f",
+    "url": "/form-project/index.html"
+  }
+];
